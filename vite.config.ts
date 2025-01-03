01b95f2fb19ae57bcd53509d18e69fb9ce2f2cc5
DhruvBagga47 <dhruv.bagga47@gmail.com>
2025-01-02 22:52:47 -0500

    Updated vite.config.js for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/DhruvBagga47/Personal-Portfolio.git',
+  base: '/Personal-Portfolio/',
 })
